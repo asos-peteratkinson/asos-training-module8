85108b509cc1bce3a48991e2694e00300bd50cd5
peter.atkinson <peter.atkinson@asos.com>
2024-05-17 13:23:52 +0100

    more refactor

--- GildedRose/GildedRose/GildedRose.cs
+++ GildedRose/GildedRose/GildedRose.cs
@@ -2,6 +2,10 @@
 {
     public class GildedRose
     {
+        private const string AgedBrie = "Aged Brie";
+        private const string BackStagePasses = "Backstage passes to a TAFKAL80ETC concert";
+        private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+
         IList<Item> Items;
         private static int LOWEST_QUALITY_VALUE = 0;
         private static int HIGHEST_QUALITY_VALUE = 50;
@@ -15,8 +19,7 @@
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" 
-                    && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (!IsAgedBrie(Items[i].Name) && !IsBackstagePasses(Items[i].Name))
                 {
                     if (IsNotLegendaryItem(Items[i]))
                     {
@@ -29,7 +32,7 @@
                     {
                         Items[i].Quality += 1;
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert" 
+                        if (Items[i].Name == BackStagePasses 
                             && Items[i].SellIn < 11 
                             && Items[i].Quality < 50)
                         {
@@ -43,36 +46,46 @@
                     }
                 }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (Items[i].Name != Sulfuras)
                 {
                     Items[i].SellIn -= 1;
                 }
 
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (IsNotLegendaryItem(Items[i]))
-                            {
-                                Items[i].Quality -= 1;
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = 0;
-                        }
-                    }
-                    else
+                    if (IsAgedBrie(Items[i].Name))
                     {
                         if (Items[i].Quality < HIGHEST_QUALITY_VALUE)
                         {
                             Items[i].Quality += 1;
                         }
+
+                        continue;
+                    }
+
+                    if (!IsBackstagePasses(Items[i].Name))
+                    {
+                        if (IsNotLegendaryItem(Items[i]))
+                        {
+                            Items[i].Quality -= 1;
+                        }
+                    }
+                    else
+                    {
+                        Items[i].Quality = 0;
                     }
                 }
             }
+        }
+        
+        private bool IsAgedBrie(string name)
+        {
+            return name == AgedBrie;
+        }
+
+        private bool IsBackstagePasses(string name)
+        {
+            return name == BackStagePasses;
         }
 
         private bool IsItemQualityGreaterThanLowestQuality(int itemQuality)
@@ -82,7 +95,7 @@
 
         private bool IsNotLegendaryItem(Item item)
         {
-            return IsItemQualityGreaterThanLowestQuality(item.Quality) && item.Name != "Sulfuras, Hand of Ragnaros";
+            return IsItemQualityGreaterThanLowestQuality(item.Quality) && item.Name != Sulfuras;
         }
     }
 }
