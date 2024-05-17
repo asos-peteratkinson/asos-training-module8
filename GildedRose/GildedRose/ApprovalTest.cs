a3b8f25a120e80db2e89577cf40aa16c36d73ae9
peter.atkinson <peter.atkinson@asos.com>
2024-05-17 10:44:48 +0100

    first commit

---
+++ GildedRose/GildedRose/ApprovalTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System.Reflection;
+using System.Text;
+
+namespace GildedRose
+{
+    [TestFixture]
+    public class ApprovalTest
+    {
+        [Test]
+        public void ThirtyDays()
+        {
+            var filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "ThirtyDays.txt");
+
+            var referenceLines = File.ReadAllLines(filePath);
+
+            var fakeoutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeoutput));
+            Console.SetIn(new StringReader("\n"));
+
+            Program.Main(new string[] { });
+            var output = fakeoutput.ToString();
+
+            var outputLines = output.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+            for (var i = 0; i < Math.Min(referenceLines.Length, outputLines.Length); i++)
+            {
+                Assert.That(referenceLines[i], Is.EqualTo(outputLines[i]));
+            }
+        }
+    }
+}
---
+++ GildedRose/GildedRose/GildedRoseTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace GildedRose
+{
+    [TestFixture]
+    public class GildedRoseTest
+    {
+        [Test]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.That("fixme", Is.EqualTo(Items[0].Name));
+        }
+    }
+}
